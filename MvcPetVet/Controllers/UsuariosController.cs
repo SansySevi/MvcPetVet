22c60456a64ef17687bbac780cb7de1e06f700be
SansySevi <ssevilleja@hotmail.es>
2023-03-03 14:23:49 +0100

    20230303

---
+++ MvcPetVet/Controllers/UsuariosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcPetVet.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcPetVet/Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace MvcPetVet.Models
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public string Apodo { get; set; }
+        public string Email { get; set; }
+        public string Contrasenia { get; set; }
+    }
+}
